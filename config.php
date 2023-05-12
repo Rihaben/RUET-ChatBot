ecd2dd34e0887c3c849dc8e29574c79f43191345
Rihaben <94992525+Rihaben@users.noreply.github.com>
2023-05-12 15:56:20 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
 
-$host="127.0.0.1";
+$host="127.29.12.43";
 $user="root";
 $pass="password";
 $db="chat";
 
 $con=mysqli_connect($host,$user,$pass,$db);
 
-//if(!$con)
-//{
-//	print("Not Connected<br>".mysqli_error());
+if(!$con)
+{
+	print("Not Connected<br>".mysqli_error());
 
-//}
-//else
-//{
-//	echo("Connected");
-//}
+}
+else
+{
+	echo("Connected");
+}
 
 
 
