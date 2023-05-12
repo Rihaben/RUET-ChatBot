e8c98a899b099fa231685d112701e1c20500d062
Rihaben <94992525+Rihaben@users.noreply.github.com>
2023-05-12 16:29:03 +0100

    Update config.php

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ $con=mysqli_connect($host,$user,$pass,$db);
 //}
 
 // Check connection
-if (!$conn) {
+if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
