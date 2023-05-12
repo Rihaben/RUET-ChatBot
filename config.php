3c6d52d202c7f43f8e4a257da4ed2be3fdfb6dff
Rihaben <94992525+Rihaben@users.noreply.github.com>
2023-05-12 16:57:41 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $password = "password";
 $dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
